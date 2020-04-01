4d1a3e0b6bc0fc3bda8803dc7b2dfae716cc5311
Santiago Ramos <santiagoramos198@gmail.com>
2020-04-01 15:43:22 -0600

    Vamos avanzando en el proyecto del registro de asistentes

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import confLogo from '../images/badge-header.svg';
+import './Styles/Badge.css';
+
 class Badge extends React.Component {
     render(){
-        return (
-            <h1>Badge</h1>
-        )
+        return <div className='Badge'>
+            <div className='Badge_header'>
+                <img src={confLogo} alt="Logo de la Conferencia"/>
+            </div>
+            <div className="Badge_section-name">
+                <img className="Badge_avatar" src={this.props.avatarUrl} alt="Avatar"/>
+            <h1>{this.props.firstName} <br/> {this.props.lastName}</h1>
+            </div>
+            <div className="Badge_section-info">
+                <h3>{this.props.profesion}</h3>
+                <div>@{this.props.twitter}</div>
+            </div>
+            <div className="Badge_footer">
+                <p>#PlatziConf</p>
+            </div>
+            </div>     
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/badge'
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
 const name = 'Santiago';
 // const jsx = <h1>Hello, Platzi Badges!</h1>;// Esto es JSX
 // const elemento = React.createElement('h1',{},`HOLA, Soy ${name}`);// Esto es React
 // const elemento = <h1>Hola, Soy {name}</h1>
-const elemento = (
-<div>
-<h1>Hola, Soy {name}</h1>
-<p>Soy Desarrollador Frontend</p>
-</div>
-);
+// const elemento = (
+// <div>
+// <h1>Hola, Soy {name}</h1>
+// <p>Soy Desarrollador Frontend</p>
+// </div>
+// );
 const container = document.getElementById('app');
 
-ReactDOM.render(elemento,container);
+ReactDOM.render(<Badge 
+    firstName='Santiago' 
+    lastName='Ramos Morales'
+    profesion='Desarrollador Frontend'
+    twitter='santiagoramossr'
+    avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+        />,container);
